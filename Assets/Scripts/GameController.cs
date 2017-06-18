6d57621239ef97010910afd46143558e6f77f050
Dmitry Kiselev <sol.linux@gmail.com>
2017-06-18 18:16:39 +0300

    Игрок бегает и прыгает

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 
-namespace TempleRun.Assets.Scripts {
+namespace TempleRun {
     public class GameController : MonoBehaviour {
-        public Hero player;
+        public CharacterController Controller;
+        public Vector3 JumpVelocity = new Vector3(0, 8.0f, 0);
+        public Vector3 Gravity = new Vector3(0, -9.8f);
+        public Vector3 Acceleration = new Vector3(0.1f, 0, 0);
+        public Vector3 Velocity;
+
+        private float verticalSpeed;
 
         public void Start() {
-            player.Run();
+            
         }
 
         public void Update() {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                player.Jump();
+            if (Controller.isGrounded) {
+                Velocity.y = 0;
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    Velocity += JumpVelocity;
+                }
             }
+            Velocity += (Acceleration+Gravity) * Time.deltaTime;
+            Controller.Move(Velocity * Time.deltaTime);
         }
     }
 }
